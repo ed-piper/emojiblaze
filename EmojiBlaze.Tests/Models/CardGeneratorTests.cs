9fa497dc056698425b126f5c62b3c658e25f52f6
edpisco <georgia74295?git>
2019-09-07 14:21:54 +0100

    Display cards on screen

--- EmojiBlaze.Tests/Models/CardGeneratorTests.cs
+++ EmojiBlaze.Tests/Models/CardGeneratorTests.cs
@@ -1,7 +1,25 @@
-﻿namespace EmojiBlaze.Tests.Models
+﻿using EmojiBlaze.Web.Models.Board;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Shouldly;
+
+namespace EmojiBlaze.Tests.Models
 {
+    [TestClass]
     public class CardGeneratorTests
     {
-        
+        private CardGenerator _sut;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _sut = new CardGenerator();
+        }
+
+        [TestMethod]
+        public void GenerateCards_WithWidthOf8_Generates64Cards()
+        {
+            var result = _sut.GenerateCards(8);
+            result.Count.ShouldBe(64);
+        }
     }
 }

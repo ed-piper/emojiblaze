f99f951fcf0cb4749e85768bf578661255042806
edpisco <georgia74295?git>
2019-09-08 08:13:22 +0100

    Add flip card functionality

---
+++ EmojiBlaze.Web/Store/Game/Actions/FlipCardAction.cs
@@ -0,0 +1,12 @@
+﻿namespace EmojiBlaze.Web.Store.Game.Actions
+{
+    public class FlipCardAction
+    {
+        public FlipCardAction(Card card)
+        {
+            Card = card;
+        }
+
+        public Card Card { get; }
+    }
+}
---
+++ EmojiBlaze.Web/Store/Game/Reducers/FlipCardReducer.cs
@@ -0,0 +1,17 @@
+﻿using Blazor.Fluxor;
+using EmojiBlaze.Web.Store.Game;
+using EmojiBlaze.Web.Store.Game.Actions;
+using EmojiBlaze.Web.Util;
+
+namespace Pairs.Store.Game.Reducers
+{
+    public class FlipCardReducer : Reducer<GameState, FlipCardAction>
+    {
+        public override GameState Reduce(GameState state, FlipCardAction action)
+        {
+            var gameState = state.Clone();
+            gameState.Cards.Find(x => x == action.Card).IsFaceDown = !action.Card.IsFaceDown;
+            return gameState;
+        }
+    }
+}

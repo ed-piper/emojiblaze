4a8b7b344d528887788b9bf0eef451d04d74b0c3
edpisco <georgia74295?git>
2019-09-25 22:37:06 +0100

    Prevent duplicate player name. Layout tweaks

--- EmojiBlaze.Web/Store/Game/Reducers/AddPlayerReducer.cs
+++ EmojiBlaze.Web/Store/Game/Reducers/AddPlayerReducer.cs
@@ -1,4 +1,5 @@
-﻿using Blazor.Fluxor;
+﻿using System.Linq;
+using Blazor.Fluxor;
 using EmojiBlaze.Web.Store.Game;
 using EmojiBlaze.Web.Store.Game.Actions;
 using EmojiBlaze.Web.Util;
@@ -10,7 +11,15 @@ namespace Pairs.Store.Game.Reducers
         public override GameState Reduce(GameState state, AddPlayerAction action)
         {
             var gameState = state.Clone();
-            gameState.Players.Add(new Player(action.Name));
+            if (gameState.Players.Any(x => x.Name.ToLower().Trim() == action.Name))
+            {
+                //TODO: Players can't have matching names, so present some sort of error message                 
+            }
+            else
+            {
+                gameState.Players.Add(new Player(action.Name));    
+            }
+            
             return gameState;
         }
     }
